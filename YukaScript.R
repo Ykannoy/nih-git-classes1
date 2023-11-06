153b544718f45198e5fb2a5b424f378a4d511957
YKannoy <kannoy@mail.nih.gov>
2023-11-06 14:39:19 -0500

    created script for training

---
+++ YukaScript.R
@@ -0,0 +1,18 @@
+install.packages("tidyverse")
+
+install.packages("gapminder")
+library(gapminder)
+
+library(tidyverse)
+
+
+read_data <- gapminder
+
+# let us add some folders
+
+dir.create("images")
+dir.create("docs")
+dir.create("figures")
+
+# last comment before commit
+
